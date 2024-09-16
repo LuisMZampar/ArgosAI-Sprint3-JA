53a1190b2f2f85672ac27f498d3946074a88d761
Luis Fernando Menezes Zampar <126732925+LuisMZampar@users.noreply.github.com>
2024-09-15 21:48:00 -0300

    Update CorsConfig.java

--- src/main/java/br/com/argos/argosaisprint3/config/CorsConfig.java
+++ src/main/java/br/com/argos/argosaisprint3/config/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig implements WebMvcConfigurer {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:8080")
+                        .allowedOrigins("http://localhost:8080, https://argos-rm550531.azurewebsites.net")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
